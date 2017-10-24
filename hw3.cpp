514bcde44b05482067e87955d4f385949b18f9cb
JuliaZY <yaozhu@usc.edu>
2017-10-24 15:21:52 -0700

    这次要是再传不上去就不许吃晚饭

---
+++ hw3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+    	if(moves.length() == 0)  return true;
+    	if(moves.length() %2 != 0)  return false;
+    int x=0; 
+    int y=0;
+        for (int i=0; i < moves.length(); i++)
+        {   if (moves[i] == 'U') y++;
+            if (moves[i] == 'D') y--;
+            if (moves[i] == 'L') x--;
+            if (moves[i] == 'R') x++;
+        }
+    if (x==0 && y==0) return true;
+     return false;
+        
+    }
+};
+
+
+
