7322496636586c6952f7b48dd12ed160174f0120
JuliaZY <yaozhu@usc.edu>
2017-10-24 21:07:15 -0700

    ammended

--- hw3.cpp
+++ hw3.cpp
@@ -7,16 +7,18 @@ public:
     bool judgeCircle(string moves) {
     	if(moves.length() == 0)  return true;
     	if(moves.length() %2 != 0)  return false;
-    int x=0; 
-    int y=0;
+        
+        int x=0; 
+        int y=0;
         for (int i=0; i < moves.length(); i++)
-        {   if (moves[i] == 'U') y++;
+        {   
+            if (moves[i] == 'U') y++;
             if (moves[i] == 'D') y--;
             if (moves[i] == 'L') x--;
             if (moves[i] == 'R') x++;
         }
-    if (x==0 && y==0) return true;
-     return false;
+        if (x==0 && y==0) return true;
+        return false;
         
     }
 };
